c1f2b46b298891b43bb1156741b00779898e2032
“pasqualeobject” <objectpas@gmail.com>
2018-03-13 19:37:12 +0100

    ok.. fatto

---
+++ Hello1/src/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+	
+	public static int doppio(int n)
+	{
+		int a = n * 2;
+		return a;
+	}
+	
+	public static void main(String[] args) {
+		int dop = doppio(4);
+		System.out.println(dop);
+		
+	}
+}
